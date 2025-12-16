5b8a48ed5eb415f7e416258b8c7902bc59f82beb
sidd118 <siddharth.sheth118@gmail.com>
2025-12-16 19:07:48 +0530

    setup: e2b sandboxes using docker

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
+import { Sandbox } from '@e2b/code-interpreter';
 import { createAgent, openai } from '@inngest/agent-kit';
 import { inngest } from "./client";
+import { getSandbox } from './utils';
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
 
-  async ({ event }) => {
+  async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-sidd-test");
+      return sandbox.sandboxId;
+    })
 
     const codeWriterAgent = createAgent({
       name: 'Code writer',
@@ -19,6 +26,12 @@ export const helloWorld = inngest.createFunction(
       'Write a button component code',
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = await sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return { output, sandboxUrl };
   },
 );
